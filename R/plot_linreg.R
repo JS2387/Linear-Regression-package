43f3dcc4a8cd8879c945989d5c441d8dd7ec1ae9
JS2387 <jaskirat23387@gmail.com>
2021-11-29 15:37:18 +0100

    fixed warning for ggplot2 in plot_linreg

--- R/plot_linreg.R
+++ R/plot_linreg.R
@@ -14,7 +14,7 @@ plot <- function (x, y, ...) UseMethod("plot")
 plot.linreg <- function (x, y, ...) {
                           # Create the DF used for plotting
                           #library(latex2exp)
-                          library(ggplot2)
+                          #library(ggplot2)
                           error_hat <- x$Residuals
                           y_hat <- x$Fittedvalues
                           df_1 <- data.frame(error_hat , y_hat)
@@ -23,7 +23,7 @@ plot.linreg <- function (x, y, ...) {
                           smoothed <- stats::lowess(x = y_hat, y = error_hat)
 
                           # Create the first plot
-                          plot_res_vs_fitted <- ggplot(df_1 , aes(x=y_hat , y=error_hat) )+
+                          plot_res_vs_fitted <- ggplot2::ggplot(df_1 , aes(x=y_hat , y=error_hat) )+
                             geom_point(shape = 1 , size=4) +
                             geom_line(aes(x = smoothed$x, y = smoothed$y), color = "red") +
                             ggtitle("Residuals vs Fitted") +
@@ -50,7 +50,7 @@ plot.linreg <- function (x, y, ...) {
                           df_2 <- data.frame(y_hat , standard_res_sqrt )
 
 
-                          plot_scale_location  <- ggplot(df_2 , aes(x=y_hat , y= standard_res_sqrt ))+
+                          plot_scale_location  <- ggplot2::ggplot(df_2 , aes(x=y_hat , y= standard_res_sqrt ))+
                             geom_point(shape = 1 , size=4) +
                             geom_line(aes(x = smoothed_2$x, y = smoothed_2$y), color = "red") +
                             ggtitle("Scale-Location") +
